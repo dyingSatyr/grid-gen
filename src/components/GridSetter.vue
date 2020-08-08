5dd8bbe3c37b8cdcc6112bcdf0a39f1de3f4d327
Boris Grabovac <grabovacb86@gmail.com>
2020-08-08 13:18:18 +0200

    grid setter pretty much wrapped up

--- src/components/GridSetter.vue
+++ src/components/GridSetter.vue
@@ -4,23 +4,31 @@
     <div class="grid-sections-picker">
       <ul>
         <li v-for="(section, index) in gridSections" :key="index">
-          <button @click="selectSection(section)" :class="section">{{section}}</button>
+          <button
+            @click="selectSection(section)"
+            :class="section.name"
+            :style="{'background-color': section.color}"
+          >{{section.name}}</button>
         </li>
       </ul>
     </div>
     <h1>Grid areas:</h1>
     <div class="grid-picker">
-      <div class="grid-picker-row" v-for="(row, index) in gridGenerated" :key="index">
+      <div class="grid-picker-row" v-for="(row, rowIndex) in gridGenerated" :key="rowIndex">
         <div
           class="grid-picker-box"
           :class="element.name"
           v-for="(element, index) in row"
           :key="index"
-          @click="setArea(element.name)"
+          @click="setArea(element, rowIndex)"
         >{{element.name}}</div>
       </div>
     </div>
-    <button @click="generateCode" :disabled="!readyToGenerateCode">Generate Code</button>
+    <button
+      class="btn-generate"
+      @click="generateCode"
+      :disabled="!readyToGenerateCode"
+    >Generate Code</button>
   </div>
 </template>
 
@@ -39,6 +47,7 @@ export default {
     };
   },
   created() {
+    //Generate grid data
     let generateGrid = (gridCnf) => {
       for (let i = 0; i < gridCnf.rows; i++) {
         for (let j = 0; j < gridCnf.columns; j++) {
@@ -50,38 +59,76 @@ export default {
             name: `area-${i + 1}-${j + 1}`,
             row: i,
             column: j,
+            section: null, //This will be filled in with appropriate section once clicked
           };
         }
       }
     };
-
+    //Generate sections (divs, sections, asides)
     let generateSections = (gridCnf) => {
       for (let i = 0; i < gridCnf.areas; i++) {
-        this.gridSections.push(`section-${i + 1}`);
+        this.gridSections.push({
+          name: `section-${i + 1}`,
+          color: this.getRandomColor(),
+        });
       }
     };
     generateSections(this.gridConfig);
     generateGrid(this.gridConfig);
   },
   methods: {
+    //helper func to generate different colors for each section
+    getRandomColor: function () {
+      let letters = "0123456789ABCDEF";
+      let color = "#";
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
+    //Finish step 2 and move on to showing code
     generateCode: function () {
       console.log("generate");
     },
-    setArea: function (section) {
+    setArea: function (area, row) {
+      //Do nothing if no section is already selected
       if (!this.selectedSection) {
         console.log("No section selected.");
         return;
       }
-      console.log(`${section} clicked`);
+      //Set color to match the section
+      let element = document.querySelector("." + area.name);
+      element.style.backgroundColor = this.selectedSection.color;
+      //Assign a section to data in generatedGrid
+      let gEle = this.gridGenerated[row].find((gridBox) => {
+        return gridBox.name === area.name;
+      });
+      gEle.section = this.selectedSection.name;
+      this.readyToGenerateCodex();
     },
     selectSection: function (section) {
+      //Check if something already selected and deselect it
       let alreadySelected = document.querySelector(".selected");
       if (alreadySelected) {
         alreadySelected.classList.remove("selected");
       }
-      let element = document.querySelector("." + section);
+      //Add selected class to element
+      let element = document.querySelector("." + section.name);
       element.classList.add("selected");
+      //Set the element as selected in data
       this.selectedSection = section;
+    },
+    //Check if all areas have sections assigned
+    readyToGenerateCodex: function () {
+      //Check if any sections are null
+      for (let i = 0; i < this.gridGenerated.length; i++) {
+        let isEmpty = this.gridGenerated[i].some((obj) => obj.section === null);
+        if (isEmpty) {
+          this.readyToGenerateCode = false;
+          return;
+        }
+      }
+      this.readyToGenerateCode = true;
     },
   },
 };
@@ -139,15 +186,24 @@ export default {
 
 .grid-sections-picker ul li {
   display: inline-block;
-  margin: 0 5px;
+}
+
+.grid-sections-picker ul li button {
   font-size: 13px;
+  border: 2px solid #f0f0f0;
+  margin: 0 10px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+  outline: none;
 }
 
 .grid-sections-picker ul li button.selected {
-  background-color: #fe638f;
+  border-color: #373737;
 }
 
-button {
+button.btn-generate {
   margin: 10px 0;
   background-color: #14c3e6;
   border: 0;
@@ -160,16 +216,16 @@ button {
   transition: all 0.2s ease-in-out;
 }
 
-button:disabled {
+button.btn-generate:disabled {
   background: #d1d1d1;
   cursor: unset;
 }
 
-button:disabled:hover {
+button.btn-generate:disabled:hover {
   background: #d1d1d1;
 }
 
-button:hover {
+button.btn-generate:hover {
   background-color: #14b3d2;
 }
 </style>
