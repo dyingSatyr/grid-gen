19d59606ca8fd2ac056e882463c97686483af548
Boris Grabovac <grabovacb86@gmail.com>
2020-08-12 10:41:38 +0200

    code generator finished

--- src/components/CodeGenerator.vue
+++ src/components/CodeGenerator.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>{{generatedHTML}}</div>
+  <div id="wrapper">
+    <div class="code-display">
+      <h1>index.html:</h1>
+      <vue-code-highlight language="html">{{generatedHTML}}</vue-code-highlight>
+    </div>
+    <div class="code-display">
+      <h1>grid.css:</h1>
+      <vue-code-highlight language="css">{{generatedCSS}}</vue-code-highlight>
+    </div>
+  </div>
 </template>
 <script>
+import { component as VueCodeHighlight } from "vue-code-highlight";
+import "prism-es6/components/prism-markup-templating";
+import "prism-es6/components/prism-markup";
+import "prism-es6/components/prism-css";
 export default {
   name: "CodeGenerator",
   props: {
@@ -10,10 +23,14 @@ export default {
   data: function () {
     return {
       generatedHTML: "",
+      generatedCSS: "",
     };
   },
+  components: {
+    VueCodeHighlight,
+  },
   created: function () {
-    console.dir(this.finalGrid);
+    //console.dir(this.finalGrid);
     let generateHTML = () => {
       let header = `
 <!DOCTYPE html>
@@ -21,6 +38,7 @@ export default {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="grid.css" type="text/css">
     <title>Grid Gen</title>
 </head>
 <body>
@@ -42,13 +60,88 @@ export default {
 `;
         }
       });
-      // console.log(header, content, footer);
+      //console.log(header, content, footer);
       this.generatedHTML = header + content + footer;
     };
+
+    let generateCSS = () => {
+      //console.log(this.finalGrid);
+
+      //Add container styles
+      let contentCSS = `
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  display:grid;
+  min-height:100vh;
+  grid-template-columns:`;
+      //Add grid template columns for each column
+      for (let i = 0; i < this.finalGrid.gridGenerated[0].length; i++) {
+        contentCSS += ` 1fr`;
+      }
+      contentCSS += `;`;
+
+      //Add grid template areas
+      contentCSS += `
+  grid-template-areas: `;
+
+      this.finalGrid.gridGenerated.forEach((row) => {
+        contentCSS += `
+        "`;
+        row.forEach((obj, index) => {
+          if (index == row.length - 1) {
+            contentCSS += `${obj.section}`;
+          } else {
+            contentCSS += `${obj.section} `;
+          }
+        });
+        contentCSS += `"`;
+      });
+      contentCSS += `;
+}
+`;
+
+      //Assign area for each section
+      this.finalGrid.sections.forEach((section) => {
+        contentCSS += `
+.${section.name} {
+  grid-area: ${section.name};
+  background-color: ${section.color};
+}
+`;
+      });
+
+      this.generatedCSS = contentCSS;
+    };
+    generateCSS();
     generateHTML();
   },
 };
 </script>
 
 <style scoped>
+#wrapper {
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  max-width: 1200px;
+  margin: 0 auto;
+  font-family: "Montserrat", sans-serif;
+}
+
+#wrapper h1 {
+  font-weight: 600;
+  font-size: 20px;
+}
+
+.code-display {
+  margin: 20px;
+}
+
+.code-display > div {
+  width: 600px;
+}
 </style>
--- src/components/GridSetter.vue
+++ src/components/GridSetter.vue
@@ -36,7 +36,7 @@
 export default {
   name: "GridSetter",
   props: {
-    gridConfig: Array,
+    gridConfig: Object,
   },
   data: function () {
     return {
